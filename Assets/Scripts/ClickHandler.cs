ba4ab04956cbff545891f55877588d5c8adf348b
Trevor <trevku155@gmail.com>
2019-02-12 22:13:52 -0500

    Finding closest unit to touched position
    Using Physics2D.OverlapCircle() to detect closest unit to touched position, instead of doing this process manually

--- Assets/Scripts/ClickHandler.cs
+++ Assets/Scripts/ClickHandler.cs
@@ -4,85 +4,63 @@ using System.Collections.Generic;
 
 public class ClickHandler : MonoBehaviour {
     // ---------- Fields ----------
-    private static readonly float CANDIDATE_DISTANCE = 100.0f;
+    private static readonly float CLICK_SEARCH_RADIUS = 1.2f;
+    private static readonly bool ISCLICK = true;   // temp control variable
 
     public Transform clickCircle;
     public Transform clickCirclePosition;
 
+    private bool screenIsTouched = false;
+
     // ---------- Methods ----------
     private void Update() {
         if (ScreenTouched()) {
-            Vector2 touchedPosition = Input.GetTouch(0).position;
-
-            // display a circle where the user clicked
-            clickCirclePosition.position = Camera.main.ScreenToWorldPoint(touchedPosition);
-            Vector3 tempPosition = clickCirclePosition.position;
-            tempPosition[2] = 0;
-            clickCirclePosition.position = tempPosition;
-            Transform spawnedCircleObject = Instantiate(clickCircle, clickCirclePosition);
-
-            
-            List<Transform> nearbyClickableUnitTransforms = GetNearbyClickableUnitTransforms(touchedPosition);
-            if (nearbyClickableUnitTransforms.Count == 0) {
+            if (screenIsTouched) {
                 return;
             }
-            IClickableUnit unitClicked = GetClosestClickableUnit(touchedPosition, nearbyClickableUnitTransforms);
+            screenIsTouched = true;
+            Vector2 touchedPosition = GetTouchedPosition();
 
-            // Decide what to do based on the type of the unit clicked
-            string unit = unitClicked.GetUnitTypeString();
+            IClickableUnit unitSelection = GetClosestClickableUnitToPosition(touchedPosition);
 
-            
+            // TODO pull up a UI panel displaying this info
+            // TODO store unit selection, give ability to move
+            string unit = unitSelection.GetUnitTypeString();
+            Debug.Log(unit);
+        } else {
+            screenIsTouched = false;
         }
     }
 
     private bool ScreenTouched() {
-        return Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began;
+        if (ISCLICK) {
+            return Input.GetMouseButton(0);
+        } else {
+            return Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began;
+        }
     }
 
-    private List<Transform> GetNearbyClickableUnitTransforms(Vector2 referencePosition) {
-        List<Transform> allNearbyClickableUnitTransforms = new List<Transform>();
-
-        GameObject[] playerUnits = GameObject.FindGameObjectsWithTag("PlayerUnit");
-        foreach (GameObject playerUnit in playerUnits) {
-            Vector2 playerUnitScreenPosition = Camera.main.WorldToScreenPoint(playerUnit.transform.position);
-            float playerUnitDistanceToReference = Vector2.Distance(referencePosition, playerUnitScreenPosition);
-            if (playerUnitDistanceToReference < CANDIDATE_DISTANCE) {
-                allNearbyClickableUnitTransforms.Add(playerUnit.transform);
-            }
+    private Vector2 GetTouchedPosition() {
+        if (ISCLICK) {
+            return Input.mousePosition;
+        } else {
+            return Input.GetTouch(0).position;
         }
-
-        GameObject[] enemyUnits = GameObject.FindGameObjectsWithTag("EnemyUnit");
-        foreach (GameObject enemyUnit in enemyUnits) {
-            Vector2 enemyUnitScreenPosition = Camera.main.WorldToScreenPoint(enemyUnit.transform.position);
-            float enemyUnitDistanceToReference = Vector2.Distance(referencePosition, enemyUnitScreenPosition);
-            if (enemyUnitDistanceToReference < CANDIDATE_DISTANCE) {
-                allNearbyClickableUnitTransforms.Add(enemyUnit.transform);
-            }
-        }
-
-        return allNearbyClickableUnitTransforms;
     }
 
-    private IClickableUnit GetClosestClickableUnit(Vector2 touchedPosition, List<Transform> closeUnits) {
-        if (closeUnits.Count == 0) {
-            return null;
-        }
-        
-        int closestUnitIndex = 0;
-        float closestUnitDistance = float.MaxValue;
-        for (int unitIndex = 0; unitIndex < closeUnits.Count; unitIndex++) {
-            Transform unit = closeUnits[unitIndex];
-            Vector2 unitScreenPosition = Camera.main.WorldToScreenPoint(unit.position);
-            float unitDistanceToReference = Vector2.Distance(touchedPosition, unitScreenPosition);
-            Debug.Log("Distance of unit [" + unit.gameObject.name + "] = " + unitDistanceToReference);
-            if (unitDistanceToReference < closestUnitDistance) {
-                closestUnitIndex = unitIndex;
-                closestUnitDistance = unitDistanceToReference;
-            }
-        }
+    private Transform SpawnCircleOnPosition(Vector2 touchedPosition) {
+        clickCirclePosition.position = Camera.main.ScreenToWorldPoint(touchedPosition);
+        Vector3 tempPosition = clickCirclePosition.position;
+        tempPosition[2] = 0;
+        clickCirclePosition.position = tempPosition;
+        Transform spawnedCircle = Instantiate(clickCircle, clickCirclePosition);
+        return spawnedCircle;
+    }
 
-        Transform closestUnit = closeUnits[closestUnitIndex];
-        IClickableUnit closestUnitComponent = closestUnit.gameObject.GetComponent<IClickableUnit>();
-        return closestUnitComponent;
+    IClickableUnit GetClosestClickableUnitToPosition(Vector2 touchedPosition) {
+        Transform spawnedCircle = SpawnCircleOnPosition(touchedPosition);
+        Collider2D closestClickableUnitToPosition = Physics2D.OverlapCircle(spawnedCircle.position, CLICK_SEARCH_RADIUS);
+        IClickableUnit unitClicked = closestClickableUnitToPosition.gameObject.GetComponent<IClickableUnit>();
+        return unitClicked;
     }
 }
