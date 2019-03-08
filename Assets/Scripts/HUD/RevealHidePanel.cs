f21f3b2f3e2ed2d2d3065e5480acf06694e8239e
Trevor <trevku155@gmail.com>
2019-03-08 00:19:06 -0500

    Buttons to reveal / hide panels
    Shop. upgrade, and Admin panels can be toggled on and off

---
+++ Assets/Scripts/HUD/RevealHidePanel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class RevealHidePanel : MonoBehaviour {
+    public Transform ShopPanel;
+    public Transform UpgradePanel;
+    public Transform AdminPanel;
+
+    public void ToggleShowShopPanel() {
+        UpgradePanel.gameObject.SetActive(false);
+        AdminPanel.gameObject.SetActive(false);
+
+        bool isActive = ShopPanel.gameObject.activeSelf;
+        ShopPanel.gameObject.SetActive(!isActive);
+    }
+
+    public void ToggleShowUpgradePanel() {
+        ShopPanel.gameObject.SetActive(false);
+        AdminPanel.gameObject.SetActive(false);
+
+        bool isActive = UpgradePanel.gameObject.activeSelf;
+        UpgradePanel.gameObject.SetActive(!isActive);
+    }
+
+    public void ToggleShowAdminPanel() {
+        UpgradePanel.gameObject.SetActive(false);
+        AdminPanel.gameObject.SetActive(false);
+
+        bool isActive = AdminPanel.gameObject.activeSelf;
+        AdminPanel.gameObject.SetActive(!isActive);
+    }
+}
