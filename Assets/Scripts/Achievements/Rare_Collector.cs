b9535ece9b281ef0f500fe863645ce1beae343b1
Trevor <trevku155@gmail.com>
2019-03-25 22:35:23 -0400

    Rare Collector Achievement

--- Assets/Scripts/Achievements/Rare_Collector.cs
+++ Assets/Scripts/Achievements/Rare_Collector.cs
@@ -6,10 +6,23 @@ public class Rare_Collector : Achievement {
     }
 
     public override bool CheckCondition() {
-        return false;
+        int numRares = 0;
+
+        GameObject[] playerUnits = GameObject.FindGameObjectsWithTag("PlayerUnit");
+        foreach (GameObject g in playerUnits) {
+            PlayerUnit playerUnit = g.GetComponent<PlayerUnit>();
+            if (playerUnit.unitClass == UnitClass.MAGIC || playerUnit.unitClass == UnitClass.FLAME) {
+                numRares++;
+            }
+        }
+
+        return numRares >= 5;
     }
 
     public override void GiveReward() {
-        throw new System.NotImplementedException();
+        GameEngine.GetInstance().messageQueue.PushMessage("Bonus: 1 B Rank Token, 1 A Rank Token");
+        GameEngine.GetInstance().messageQueue.PushMessage(this.achievementName + " Complete");
+        GameEngine.GetInstance().AddBBonusTokens(1);
+        GameEngine.GetInstance().AddABonusTokens(1);
     }
 }
